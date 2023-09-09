490be65e94223b14ef163b08c7fcf1ad176a08af
Sebastian Asprino <asprinosebastian@proton.me>
2023-09-08 22:24:55 -0500

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import test from './assets/test.jpg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,24 +10,28 @@ function App() {
   return (
     <>
       <div>
+        <h1>Sebastian Asprino</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+
+        <a href="https://github.com/SebastianAsprino" target="_blank">
+          <img src={test} className="logo test" alt="test" />
+        </a>
+
+
+
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        en desarrollo
       </p>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+function Main() {
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    // Agrega la clase 'appear' después de 1 segundo para que el contenido aparezca gradualmente
+    const timer = setTimeout(() => {
+      setShowContent(true);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className={`app-container ${showContent ? 'appear' : ''}`}>
+      <App />
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
-)
+);
